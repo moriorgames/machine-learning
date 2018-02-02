ced9af293e807823893403a3e21f1b87ed0bda75
Morior <moriorgames@gmail.com>
2018-02-02 07:38:21 +0100

    Feed Forward propagation

--- src/MoriorGames/Entity/Neuron.cpp
+++ src/MoriorGames/Entity/Neuron.cpp
@@ -2,13 +2,26 @@
 
 using MoriorGames::Neuron;
 
-MoriorGames::Neuron::Neuron(unsigned nextLayer)
+Neuron::Neuron(unsigned index, unsigned nextLayer)
+    : index{index}
 {
     cout << "Made a Neuron" << endl;
-    cout << "nextLayer: " << nextLayer << endl;
+    cout << "Index: " << index << " nextLayer: " << nextLayer << endl;
     for (unsigned i = 0; i < nextLayer; ++i) {
         outputWeights.push_back(new Connection);
     }
+}
+
+void Neuron::feedForward(Layer *layer)
+{
+    double sum = 0.0;
+
+    // Sum the previous layer outputs and add the Bias Neuron output
+    for (unsigned i = 0; i < layer->size(); ++i) {
+        cout << "i: " << i << " Index: " << index << endl;
+        sum += layer->at(i)->getOutput() * layer->at(i)->getOutputWeights()[index]->weight;
+    }
+
 }
 
 double Neuron::getOutput() const
@@ -16,12 +29,29 @@ double Neuron::getOutput() const
     return output;
 }
 
+const vector<MoriorGames::Connection *> &Neuron::getOutputWeights() const
+{
+    return outputWeights;
+}
+
 void Neuron::setOutput(double output)
 {
     Neuron::output = output;
 }
 
-double Neuron::sigmoid(double x)
+/**
+ * We Compute hyperbolic tangent function as an Activation function
+ *
+ * @param x
+ *
+ * @return double
+ */
+double Neuron::activationFunction(double x)
 {
-    return 1.0 / (1.0 + exp(-x));
+    return tanh(x);
+}
+
+double Neuron::activationFunctionDerivative(double x)
+{
+    return 1.0 - x * x;
 }
